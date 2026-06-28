fea074de353f8b765303cddf93f3e0cf4d80fc16
Aaron Stinehour <aaron.stinehour@gmail.com>
2026-06-28 15:53:59 -0400

    add ch1 ex2 solution

---
+++ chapter_1/exercise_2.c
@@ -0,0 +1,13 @@
+/*
+EXERCISE 1-2
+
+EXPERIMENT WITH /C, WHERE "C" CAN BE SWAPPED WITH DIFFERENT CHARS
+
+THROWS A WARNING WHEN COMPILING
+*/
+
+#include <stdio.h>
+
+int main () {
+	printf("testing testing\k\n");
+}
