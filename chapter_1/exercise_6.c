b002e63152b3ff4f8790c0a047366ab21e36d413
Aaron Stinehour <aaron.stinehour@gmail.com>
2026-06-28 16:53:31 -0400

    add ch1 ex6 solution

---
+++ chapter_1/exercise_6.c
@@ -0,0 +1,14 @@
+/*
+Exercise 1-6
+
+Verify that the expression getchar() != EOF is 0 or 1
+
+Tip: The EOF character can be typed using ctrl+z on windows, or with ctrl+d on unix systems
+*/
+
+#include <stdio.h>
+
+int main () {
+	printf("value of getchar() != EOF: %d\n", getchar() != EOF);
+
+}
