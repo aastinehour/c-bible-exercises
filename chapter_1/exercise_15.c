81dbfbc706b1761e91b95c3523f3ec8bc338e567
aastinehour <aaron.stinehour@gmail.com>
2026-06-30 23:44:24 -0400

    add ch1 ex15 solution

---
+++ chapter_1/exercise_15.c
@@ -0,0 +1,26 @@
+/*
+Exercise 1-15:
+
+Rewrite the temperature conversion program of Section 1.2 to use a function for conversion.
+*/
+
+#include <stdio.h>
+
+#define MIN 0
+#define MAX 300
+#define STEP 25
+
+float fahr_to_celcius(int fahr);
+
+int main () {
+    printf("far --- cel\n");
+
+    int fahr;
+    for (fahr = 0; fahr <= MAX; fahr += STEP) {
+        printf("%3d\t%3.1f\n", fahr, fahr_to_celcius(fahr));
+    }
+}
+
+float fahr_to_celcius(int fahr) {
+    return (5.0 / 9.0) * (fahr - 32.0);
+}
