9267f4b1a24da92d0db1d88deed05ff167098f6d
Aaron Stinehour <81335718+aastinehour@users.noreply.github.com>
2026-07-20 20:23:04 +0000

    add ch1 ex23 solution

--- chapter_1/exercise_23.c
+++ chapter_1/exercise_23.c
@@ -1,1 +1,60 @@
+/*
+Exercise 1-23:
 
+Write a program to remove all comments from a C program. 
+Don't forget to handle quoted strings and charater constants properly. C comments do not nest.
+*/
+
+#include <stdio.h>
+
+#define IN_COMMENT 1
+#define OUT_COMMENT 0
+
+void handle_inline_comment(void);
+void handle_multiline_comment(void);
+
+int main(void) {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == '/') {
+            int d = getchar();
+            if (d == '/') {
+                handle_inline_comment();
+            } else if (d == '*') {
+                handle_multiline_comment();
+            } else {
+                putchar(c);
+                putchar(d);
+            }
+        } else {
+            putchar(c);
+        }
+    } 
+
+    return 0;
+}
+
+void handle_inline_comment(void) {
+    int c;
+    while ((c = getchar()) != EOF && c != '\n') {
+        ;
+    }
+
+    if ((c == '\n')) {
+        putchar(c);
+    }
+}
+
+void handle_multiline_comment(void) {
+    int comment_status = IN_COMMENT;
+
+    while (comment_status == IN_COMMENT) {
+        int c = getchar();
+        if (c == '*') {
+            int d = getchar();
+            if (d == '/') {
+                comment_status = OUT_COMMENT;
+            }
+        }
+    }
+}
