073a8d5ddbda624c57ab2747bcd53e09f5b1d69b
aastinehour <aaron.stinehour@gmail.com>
2026-08-16 13:18:05 -0400

    add alternate solution to ch2 ex6

--- chapter_2/exercise_06.c
+++ chapter_2/exercise_06.c
@@ -6,6 +6,7 @@ leaving the other bits unchanged.
 */
 
 unsigned setbits(unsigned x, int p, int n, unsigned y);
+unsigned setbits_2(unsigned x, int p, int n, unsigned y);
 
 int main(void) {
     return 0;
@@ -16,5 +17,25 @@ unsigned setbits(unsigned x, int p, int n, unsigned y) {
     unsigned mask = ~(~0 << n);
     unsigned cleared_x = ~(mask << shift) & x;
     unsigned y_bits = (y & mask) << shift;
+
     return cleared_x | y_bits;
+}
+
+/* Alternate solution */
+unsigned setbits_2(unsigned x, int p, int n, unsigned y) {
+    unsigned mask = 0;
+
+    for (int i = 0; i < n; i++) {
+        mask = mask << 1;
+        mask = mask | 1;
+    }
+
+    unsigned temp_n = y & mask;
+    temp_n = temp_n << p;
+
+    mask = mask << p;
+    mask = ~mask;
+
+    x = x & mask;
+    x = x | temp_n;
 }
---
+++ chapter_2/exercise_07.c
@@ -0,0 +1,16 @@
+/*
+Exercise 2-7:
+
+Write a function invert(x, p, n, y) that returns x with the n bits that begin at position p set to the rightmost n bits of y,
+leaving the other bits uncahnged.
+*/
+
+unsigned invert(unsigned x, int p, int n, unsigned y);
+
+int main(void) {
+    return 0;
+}
+
+unsigned invert(unsigned x, int p, int n, unsigned y) {
+    
+}
