9688eb2498a60cc8b694857265838fe04be0fe3d
aastinehour <aaron.stinehour@gmail.com>
2026-08-17 10:31:32 -0400

    fix setbits() function

--- chapter_2/exercise_06.c
+++ chapter_2/exercise_06.c
@@ -5,24 +5,15 @@ Write a function setbits(x, p, n, y) that returns x with the n bits that begin a
 leaving the other bits unchanged.
 */
 
+#include <stdio.h>
+
 unsigned setbits(unsigned x, int p, int n, unsigned y);
-unsigned setbits_2(unsigned x, int p, int n, unsigned y);
 
 int main(void) {
     return 0;
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    int shift = p + 1 - n;
-    unsigned mask = ~(~0 << n);
-    unsigned cleared_x = ~(mask << shift) & x;
-    unsigned y_bits = (y & mask) << shift;
-
-    return cleared_x | y_bits;
-}
-
-/* Alternate solution */
-unsigned setbits_2(unsigned x, int p, int n, unsigned y) {
     unsigned mask = 0;
 
     for (int i = 0; i < n; i++) {
@@ -38,4 +29,6 @@ unsigned setbits_2(unsigned x, int p, int n, unsigned y) {
 
     x = x & mask;
     x = x | temp_n;
+
+    return x;
 }
